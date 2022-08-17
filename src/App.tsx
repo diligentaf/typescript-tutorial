38c756e85c64f7c83a453c6b6a93e044e6817539
moog.kwon <diligentaf@gmail.com>
2022-08-17 18:34:52 +0900

    Type explanations done (18:00)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+let name: string;
+name = '5';
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+role = [5, 'yo'];
+let anyType: any;
+let unkwonType: unknown; // recommended over any
+
+// union in Typescript
+age = "Twenty two";
+console.log(age);
+age = 5;
+console.log(age);
+
+let functionVoid: (name: string) => void; // void returns undefined
+let functionNever: (name: string) => never; // void returns nothing
+
+// function printName(name: string) {
+//   console.log(name);
+// }
+
+// // creating a Person object
+type Person = {
+  name: string;
+  age?: number;
+};
+
+let person: Person = {
+  name: 'DiligentAF',
+  // age: 22,
+}
+
+let lotsofPoeple: Person[];
+
+type X = {
+  a: string;
+  b: number;
+}
+
+type Y = X & {
+  c: string;
+  d: number;
+}
+
+let xANDy: Y = {
+  a: 'efdas',
+  b: 42,
+  c: 'efdas',
+  d: 42,
+}
+
+interface Bmw {
+  name: string;
+  age?: number;
+};
+
+interface Car extends Bmw {
+  profession: string;
+}
+
+let car: Car = {
+  profession: 'ayyy',
+  name: 'bmw2007',
+  // age: 13
+}
+
+console.log(car);
+
+// function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {name}
     </div>
   );
 }
